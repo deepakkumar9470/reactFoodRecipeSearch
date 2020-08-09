4c7d156607ccbfad90c824dcfed03a8b3b7200a4
Deepak Kumar Chouhan <dk397787@gmail.com>
2020-08-09 10:57:44 +0530

    Updates

---
+++ precache-manifest.76137c7c00bb687f24e6036ec11fad90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041a33db35676058e91e91e269d27fb5",
+    "url": "/reactFoodRecipeSearch/index.html"
+  },
+  {
+    "revision": "04c51a220ae7601856c2",
+    "url": "/reactFoodRecipeSearch/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "556520e2b61aacf91d83",
+    "url": "/reactFoodRecipeSearch/static/css/main.a0ebf447.chunk.css"
+  },
+  {
+    "revision": "04c51a220ae7601856c2",
+    "url": "/reactFoodRecipeSearch/static/js/2.731e27e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactFoodRecipeSearch/static/js/2.731e27e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556520e2b61aacf91d83",
+    "url": "/reactFoodRecipeSearch/static/js/main.672aa465.chunk.js"
+  },
+  {
+    "revision": "70160226afcb1b337412",
+    "url": "/reactFoodRecipeSearch/static/js/runtime-main.e2e4e9f7.js"
+  }
+]);
